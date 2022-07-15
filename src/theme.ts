48f29972b90bc22bb81ca4c34eaf34dd219eff2c
unknown <sand8594@hufs.ac.kr>
2022-07-15 17:43:13 +0900

    setup

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+    bgColor: "whitesmoke",
+    textColor: "black",
+    accentColor: "#9c88ff",
+    ItemBgColor: "white",
+};
